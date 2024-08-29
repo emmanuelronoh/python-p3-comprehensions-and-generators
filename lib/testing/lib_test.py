57dd8edc0f9a5975f784a6a2c1bd93cf9b2acf7a
emmanuel ronoh <eronoh036@gmail.com>
2024-08-29 16:29:03 +0300

    first commit

--- lib/testing/lib_test.py
+++ lib/testing/lib_test.py
@@ -1,36 +1,36 @@
-#!/usr/bin/env python3
-
+import pytest
 from list_comprehension import return_evens, make_exclamation
 
 class TestReturnEvens:
-    '''function return_evens()'''
+    '''Function return_evens()'''
 
     def test_return_empty_list_if_odds(self):
-        '''returns empty list when num_list has no evens'''
-        num_list = range(1,10,2)
-        assert return_evens(num_list) == []
+        '''Returns empty list when num_list has no evens'''
+        num_list = range(1, 10, 2)  # This generates: [1, 3, 5, 7, 9]
+        assert return_evens(num_list) == []  # No even numbers, should return []
 
     def test_return_evens(self):
-        '''returns evens from num_list'''
-        num_list = range(10)
-        assert return_evens(num_list) == [0, 2, 4, 6, 8]
+        '''Returns list of even numbers from num_list'''
+        num_list = range(10)  # This generates: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        assert return_evens(num_list) == [0, 2, 4, 6, 8]  # Should return even numbers
+
 
 class TestMakeExclamation:
-    '''function make_exclamation()'''
+    '''Function make_exclamation()'''
 
     def test_return_empty_list_if_empty_input(self):
-        '''returns empty list when sentence_list is empty'''
-        assert make_exclamation([]) == []
+        '''Returns empty list when sentence_list is empty'''
+        assert make_exclamation([]) == []  # Empty input should return empty list
 
     def test_return_exclamation_list(self):
-        '''returns list of sentences with exclamation marks'''
+        '''Returns list of sentences with exclamation marks appended'''
         sentence_list = [
             "I like computers",
             "I require coffee",
             "Live long and prosper",
         ]
-        assert(make_exclamation(sentence_list) == [
+        assert make_exclamation(sentence_list) == [
             "I like computers!",
             "I require coffee!",
             "Live long and prosper!",
-        ])
+        ]  # Each sentence should have an exclamation mark appended
